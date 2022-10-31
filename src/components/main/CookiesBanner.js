f6c6dabd4d1317d72b276fc5d2c3f25518a08104
Kim Lan Phan Hoang <pyphilia@gmail.com>
2022-10-31 11:17:08 +0100

    refactor: set cookie banner domain (#142)

--- src/components/main/CookiesBanner.js
+++ src/components/main/CookiesBanner.js
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 
 import { CookiesBanner } from '@graasp/ui';
 
-import { ACCEPT_COOKIES_NAME } from '../../config/constants';
+import { ACCEPT_COOKIES_NAME, DOMAIN } from '../../config/constants';
 
 const Component = () => {
   const { t } = useTranslation();
@@ -13,6 +13,7 @@ const Component = () => {
       acceptText={t('Accept all')}
       declineButtonText={t('Reject non-essential')}
       cookieName={ACCEPT_COOKIES_NAME}
+      domain={DOMAIN}
       text={t(
         `We use cookies and other tracking technologies to improve your browsing experience on our website, to analyze our website traffic, and to understand where our visitors are coming from. By browsing our website, you consent to our use of cookies and other tracking technologies.`,
       )}
